f1f515f109d41ba4c19ff500555cf04e94faac2c
kagannikita <kagan.nikita@bk.ru>
2021-03-04 15:34:27 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function toReadable (number) {
-  
+    // register numbers and tens
+    const num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+    const tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+    if (number < 20) return num[number];// after 20 you can give numbers
+    const digit = number % 10
+    if (number < 100) return tens[~~(number/10)-2] + (digit? " " + num[digit]: "");// 21-99
+    return num[~~(number/100)] +" hundred" + (number%100 === 0? "": " " + toReadable(number%100));// 100-999
+
 }
